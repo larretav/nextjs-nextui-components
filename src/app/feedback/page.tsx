33ee36227a1862861c7a89f2a00723a7ab2725a5
Pablo <cv.juanp@gmail.com>
2024-09-04 16:25:40 -0700

    Agregado componente PackageCard para móvil

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -1,8 +1,14 @@
+import PackageMobileCard from "@/components/feedback/PackageMobileCard";
 
 export default function FeedBackPage() {
   return (
     <div>
       <h1>Feedback Components</h1>
+      <PackageMobileCard
+        title="Nike Air Force"
+        subtitle="Tenis 28cm"
+        weightMeasures="35 x 20 x 20 - 1kg"
+        quantity={4} />
     </div>
   );
 }
