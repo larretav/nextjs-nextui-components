9f6f5baade220a36f4f84cea2e74531c8e037599
Pablo <cv.juanp@gmail.com>
2024-09-04 16:46:55 -0700

    Agregado PackageCard component

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -1,8 +1,15 @@
+import PackageCard from "@/components/feedback/PackageCard";
 
 export default function FeedBackPage() {
   return (
     <div>
       <h1>Feedback Components</h1>
+      <PackageCard
+      title="Nike Air Force"
+      subtitle="Tenis 28cm"
+      weightMeasures="35 x 30 x 20 - 1kg"
+      quantity={4}
+      />
     </div>
   );
 }
---
+++ src/components/feedback/PackageCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Card, CardBody} from "@nextui-org/card";
+import Image from 'next/image';
+import boxImage from "@/assets/box.png"
+type Props = {
+    title: string
+    subtitle: string,
+    weightMeasures: string,
+    quantity: number
+}
+
+export default function PackageCard({ title, subtitle, weightMeasures, quantity }: Props) {
+    return (
+        <Card className='flex flex-row max-w-[410px] p-3'>
+            <div className='flex mx-3'>
+                <Image
+                    className="self-center"
+                    alt='caja'
+                    width={60}
+                    height={60}
+                    src={boxImage} />
+            </div>
+            <CardBody className='flex flex-row'>
+                <div className='w-full'>
+                    <p className='font-semibold text-sm'>{title}</p>
+                    <p className='text-gray-400 text-xs'>{subtitle}</p>
+                    <p className='font-bold text-xs tracking-widest'>{weightMeasures}</p>
+                </div>
+                <div className='self-center text-2xl font-bold'>
+                    X{quantity}
+                </div>
+            </CardBody>
+        </Card>
+    )
+}
