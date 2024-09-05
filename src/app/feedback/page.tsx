8b94c695bddc9ac1400d43ba29284c6676c074ac
Pablo <cv.juanp@gmail.com>
2024-09-05 10:03:04 -0700

    Actualizado componente PackageCard & pagina de feedback components

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -5,29 +5,29 @@ export default function FeedBackPage() {
   return (
     <div className="flex flex-col gap-3 px-6 py-3">
       <PageTitle text="Feedback Components" />
-
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
         <PackageCard
+          leadingIcon="box"
           title="Nike Air Force"
           subtitle="Tenis 28cm"
-          weightMeasures="35 x 30 x 20 - 1kg"
+          weightMeasures="35x30x20-1kg"
           quantity={4}
         />
         <PackageCard
-          title="Nike Air Force"
-          subtitle="Tenis 28cm"
-          weightMeasures="35 x 30 x 20 - 1kg"
-          quantity={4}
+          leadingIcon="envelope"
+          title="Documentos"
+          subtitle="Papelería"
+          weightMeasures="10x15x1-0.5kg"
+          quantity={2}
         />
         <PackageCard
-          title="Nike Air Force"
-          subtitle="Tenis 28cm"
-          weightMeasures="35 x 30 x 20 - 1kg"
-          quantity={4}
+          leadingIcon="pallet"
+          title="Llantas de carro"
+          subtitle="auto partes"
+          weightMeasures="65x60x40-10kg"
+          quantity={1}
         />
-
       </div>
-
     </div>
   );
 }
--- src/components/feedback/PackageCard.tsx
+++ src/components/feedback/PackageCard.tsx
@@ -1,28 +1,23 @@
 import React from 'react'
-import { Card, CardBody} from "@nextui-org/card";
-import Image from 'next/image';
-import boxImage from "@/assets/box.png"
+import { Card, CardBody } from "@nextui-org/card";
+import PackageType from '../icons/composed/PackageType';
 type Props = {
+    leadingIcon: "box" | "envelope" | "pallet"
     title: string
     subtitle: string,
     weightMeasures: string,
     quantity: number
 }
 
-export default function PackageCard({ title, subtitle, weightMeasures, quantity }: Props) {
+export default function PackageCard({ leadingIcon, title, subtitle, weightMeasures, quantity }: Props) {
     return (
-        <Card className='flex-row h-fit shadow-md gap-2 pl-2 p-5 py-0' >
-            <div className='flex '>
-                <Image
-                    className="self-center"
-                    alt='caja'
-                    width={40}
-                    height={40}
-                    src={boxImage} />
+        <Card className='flex-row shadow-md  gap-2 pl-2 p-5 py-0' >
+            <div className='flex'>
+                <PackageType type={leadingIcon} className='self-center' />
             </div>
             <CardBody className='flex flex-row'>
                 <div className='w-full'>
-                    <p className='font-semibold text-sm '>{title}</p>
+                    <p className='font-semibold text-sm'>{title}</p>
                     <p className='text-gray-400 text-xs'>{subtitle}</p>
                     <p className='font-bold text-xs tracking-widest'>{weightMeasures}</p>
                 </div>
