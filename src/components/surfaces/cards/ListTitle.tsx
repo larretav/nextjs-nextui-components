485f4fdb1b1985b22514ff0f9962b97b85f07b66
Pablo <cv.juanp@gmail.com>
2024-09-05 17:13:11 -0700

    Agregado componente ListTitle

---
+++ src/components/surfaces/cards/ListTitle.tsx
@@ -0,0 +1,31 @@
+import PackageType from '@/components/data-display/onsite/PackageType'
+import { Card } from '@nextui-org/card'
+import {Button} from "@nextui-org/button";
+import { FaEllipsisVertical } from 'react-icons/fa6';
+import React from 'react'
+type Props = {
+    title: string,
+    subtitle: string
+    leadingIcon?: "box" | "envelope" | "pallet"
+    showTrailing?: boolean
+}
+
+export default function ListTitle({ title, subtitle, leadingIcon,showTrailing }: Props) {
+    return (
+        <Card className='py-3 px-4 flex-row'>
+            {leadingIcon &&
+                <div className='flex mx-2'>
+                    <PackageType type={leadingIcon} className="self-center" />
+                </div>}
+            <div className='w-full'>
+                <p className='text-sm font-medium'>{title}</p>
+                <p className='text-xs leading-4 font-medium text-gray-400'>{subtitle}</p>
+            </div>
+            {showTrailing &&                
+                    <Button isIconOnly variant='light' size='sm' className='self-center'>
+                    <FaEllipsisVertical/>                    
+                    </Button>
+                }
+        </Card>
+    )
+}
