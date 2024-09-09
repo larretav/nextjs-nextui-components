24f1a91a06956c30aa969e0f7898d50540952590
Pablo <cv.juanp@gmail.com>
2024-09-09 16:14:29 -0700

    Agregado componente SimpleCounter input

---
+++ src/components/inputs/SimpleCounter.tsx
@@ -0,0 +1,35 @@
+import { Button } from '@nextui-org/button'
+import { Input } from '@nextui-org/input'
+import React, { useState } from 'react'
+import { FaMinus, FaPlus } from 'react-icons/fa6'
+
+type Props = {}
+
+export default function SimpleCounter({}: Props) {
+    const [count, setCount] = useState(0)    
+  return (
+    <div className="flex flex-col gap-2">
+    <div className="flex gap-1 ">
+        <Button isIconOnly size='sm' radius='full' variant='faded' className='w-5 h-8'
+            onPress={() => { count <= 0 ? setCount(0) : setCount(count - 1) }}                       >
+            <FaMinus className='text-slate-500 dark:text-slate-200' size={8}
+            />
+        </Button>
+        <Input variant='faded' size='sm' type='number' readOnly classNames={{ input: "text-center text-xs",base: "w-8 no-arrows" }} 
+            value={count.toString()}
+            onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <Button isIconOnly size='sm' radius='full' variant='faded' className='w-5 h-8'
+        onPress={() => setCount(count + 1)}>
+            <FaPlus className='text-slate-500 dark:text-slate-200' size={8}
+            />
+        </Button>
+    </div>
+    <Button color='danger' size='sm' variant='light' className='font-medium'
+        onClick={() => setCount(0)}
+    >
+        Remover
+    </Button>
+</div>
+  )
+}
