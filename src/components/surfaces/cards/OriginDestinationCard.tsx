520776a58a68e8214d3b3428b9be06b2dc46088d
Alejandro Larreta Valenzuela <larreta_vzla@hotmail.com>
2024-09-09 18:30:04 -0700

    Cambios post review

--- src/components/surfaces/cards/OriginDestinationCard.tsx
+++ src/components/surfaces/cards/OriginDestinationCard.tsx
@@ -1,36 +1,36 @@
 "use client"
 import React from 'react'
-import { Card, CardHeader,CardFooter, CardBody } from '@nextui-org/card'
+import { Card, CardHeader, CardFooter, CardBody } from '@nextui-org/card'
 import { RiUserSearchFill } from "react-icons/ri";
 import { MdNotListedLocation } from "react-icons/md";
 import { Button } from '@nextui-org/button';
 
 type Props = {
-    title:string,
-    postalCode:string
-    address:string
+    title: string,
+    postalCode: string
+    address: string
 }
 
-export default function OriginDestinationCard({title,postalCode,address}: Props) {
-  return (
-    <Card className='w-min px-3'>
-        <CardHeader className='p-0 py-2'>
-            <div className="flex items-center gap-2">
-                <p className="font-semibold text-default-400">{title}:</p>
-                <Button isIconOnly size='sm' radius='full' variant='light'>
-                <RiUserSearchFill className='text-default-600' size={18}/>
-                </Button>
-                <Button isIconOnly size='sm' radius='full' variant='light'>
-                <MdNotListedLocation className='text-default-600' size={18}/>
-                </Button>
-            </div>
-        </CardHeader>
-        <CardBody className='justify-center items-center p-0'>
-            <p className='border-2 rounded-xl p-3 text-4xl font-base'>{postalCode}</p>
-        </CardBody>
-        <CardFooter className='pb-4 px-2'>
-            <p className='font-medium text-sm py-1'>{address}</p>
-        </CardFooter>
-    </Card>
-  )
+export default function OriginDestinationCard({ title, postalCode, address }: Props) {
+    return (
+        <Card className='w-min px-3'>
+            <CardHeader className='p-0 py-2'>
+                <div className="flex items-center gap-2">
+                    <p className="font-semibold text-default-400">{title}:</p>
+                    <Button isIconOnly size='sm' radius='full' variant='light'>
+                        <RiUserSearchFill className='text-default-600' size={18} />
+                    </Button>
+                    <Button isIconOnly size='sm' radius='full' variant='light'>
+                        <MdNotListedLocation className='text-default-600' size={18} />
+                    </Button>
+                </div>
+            </CardHeader>
+            <CardBody className='justify-center items-center p-0'>
+                <p className='border-2 rounded-xl p-3 text-4xl font-base'>{postalCode}</p>
+            </CardBody>
+            <CardFooter className='pb-4 px-2'>
+                <p className='font-medium text-sm py-1'>{address}</p>
+            </CardFooter>
+        </Card>
+    )
 }
--- src/components/surfaces/cards/PackageItem.tsx
+++ src/components/surfaces/cards/PackageItem.tsx
@@ -2,8 +2,9 @@
 import PackageType from '@/components/data-display/onsite/PackageType'
 import { Card, CardBody } from '@nextui-org/card'
 import { Input } from "@nextui-org/input";
-import React  from 'react'
+import React from 'react'
 import SimpleCounter from '@/components/inputs/SimpleCounter';
+import { Divider } from '@nextui-org/divider';
 
 type Props = {
     content: string,
@@ -13,23 +14,28 @@ type Props = {
 export default function PackageItem({ content, packageType }: Props) {
     return (
         <Card >
-            <CardBody className='flex-row'>
-                <div className="flex flex-col justify-center">
+            <CardBody className='flex-row gap-2 items-center'>
+                <div className="flex flex-col justify-center gap-2">
                     <div className="flex gap-2 items-center">
                         <PackageType type={packageType} width={30} height={30} />
-                        <p className='font-semibold'>{content}</p>
+                        <span className='font-semibold'>{content}</span>
                     </div>
-                    <div className="flex gap-1 mt-1">
-                        <Input classNames={{ inputWrapper:"p-0",input: "text-center text-[8px]", base: "w-8 no-arrows ", helperWrapper: "text-center" }} size='sm' description="cm" variant='bordered' type='number' />
-                        <Input classNames={{ inputWrapper:"p-0",input: "text-center text-[8px]", base: "w-8 no-arrows ", helperWrapper: "text-center" }} size='sm' description="cm" variant='bordered' type='number' />
-                        <Input classNames={{ inputWrapper:"p-0",input: "text-center text-[8px]", base: "w-8 no-arrows ", helperWrapper: "text-center" }} size='sm' description="cm" variant='bordered' type='number' />
-                        <Input classNames={{ inputWrapper:"p-0",input: "text-center text-[8px]", base: "w-8 no-arrows ", helperWrapper: "text-center" }} size='sm' description="kg" variant='bordered' type='number' />
+                    <div className="flex gap-1 mt-1 flex-nowrap">
+                        <Input classNames={{ inputWrapper: "p-0", input: "text-center text-xs", base: " no-arrows ", helperWrapper: "text-center" }} size='sm' description="cm" variant='bordered' type='number' />
+                        <Input classNames={{ inputWrapper: "p-0", input: "text-center text-xs", base: " no-arrows ", helperWrapper: "text-center" }} size='sm' description="cm" variant='bordered' type='number' />
+                        <Input classNames={{ inputWrapper: "p-0", input: "text-center text-xs", base: " no-arrows ", helperWrapper: "text-center" }} size='sm' description="cm" variant='bordered' type='number' />
+                        <Input classNames={{ inputWrapper: "p-0", input: "text-center text-xs", base: " no-arrows ", helperWrapper: "text-center" }} size='sm' description="kg" variant='bordered' type='number' />
                     </div>
                 </div>
-                <div className="ml-auto mt-8">
-                <SimpleCounter/>
-                </div>
+                <Divider orientation="vertical" className="h-3/5" />
+                {/* <div className="ml-auto mt-8">
+                    <SimpleCounter />
+                </div> */}
+
+                <SimpleCounter />
+
             </CardBody>
+
         </Card>
     )
 }
