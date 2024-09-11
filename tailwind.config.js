d5ecc43fd0ff6ee0af1cda6e1469812aa1dacc81
Alejandro Larreta Valenzuela <larreta_vzla@hotmail.com>
2024-09-11 11:05:45 -0700

    Importaciones de nextui en tailwind.config borradas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const {nextui} = require('@nextui-org/theme');
-const {nextui} = require('@nextui-org/theme');
 const tailwindConfig = require("./src/tailwind.config");
 
 module.exports = tailwindConfig;
