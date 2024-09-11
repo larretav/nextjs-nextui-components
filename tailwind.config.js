c9053f766fe6708ccd9f52580e080a19992ddd57
Alejandro Larreta Valenzuela <larreta_vzla@hotmail.com>
2024-09-11 10:38:20 -0700

    Importacion de nextui doble, se eliminó

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 const {nextui} = require('@nextui-org/theme');
-const {nextui} = require('@nextui-org/theme');
 const tailwindConfig = require("./src/tailwind.config");
 
 module.exports = tailwindConfig;
