b7665a20e99dc0554bc560d915d3050d0896d6d7
Pablo <cv.juanp@gmail.com>
2024-09-10 17:22:41 -0700

    Actualizado componente BoxBadge para aceptar mas props

--- src/app/data-display/badges/page.tsx
+++ src/app/data-display/badges/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { PageTitle } from '@/components';
-import { Card } from '@nextui-org/card';
 import BoxBadge from "@/components/data-display/badge/BoxBadge"
 import React from 'react'
 
@@ -13,8 +12,11 @@ export default function Page() {
     <div className="flex flex-col p-3">
       <PageTitle text="Badges Componentes" />
         <p>BoxBadge</p>
-      <div className="relative w-full grid grid-cols-1 sm:grid-cols-4 gap-4 px-6 py-2">
-        <BoxBadge quantity={10} />
+      <div className="relative w-full grid grid-cols-9  gap-4 px-6 py-2">
+      <BoxBadge quantity={10} />
+      <BoxBadge quantity={20} badgeSize='lg' boxSize={20} badgeVariant='faded' padding='md'/>
+      <BoxBadge quantity={50} badgeSize='lg' boxSize={50} badgeVariant='faded'/>
+      <BoxBadge quantity={90} badgeSize='lg' boxSize={90} badgeVariant='shadow' padding='md'/>
       </div>
     </div>
   )
