6a35b0c05e5e7d7ea70a4727b434b7774f3eb3cc
Pablo <cv.juanp@gmail.com>
2024-09-12 18:20:01 -0700

    Agregado contenido a la página de inputs

--- src/app/inputs/page.tsx
+++ src/app/inputs/page.tsx
@@ -1,8 +1,25 @@
+import { PageTitle, ThemeSwitch, ThemeSwitchTabs } from "@/components";
+import BoxCounter from "@/components/inputs/BoxCounter";
+import SimpleCounter from "@/components/inputs/SimpleCounter";
+import { Switch } from "@nextui-org/switch";
 
 export default function InputsPage() {
   return (
-    <div>
-      <h1>Inputs Components</h1>
+    <div className="flex flex-col gap-3 py-3 px-6">
+      <PageTitle text="Input Components" />
+      <p className="font-medium p-2">SimpleCounter</p>
+      <div className="w-min">
+      <SimpleCounter/>
+      </div>
+      <p className="font-medium p-2">Box Counter</p>
+      <div className="w-min">
+      <BoxCounter/>
+      </div>
+      <div className="grid grid-cols-2 gap-2 md:grid-cols-12">
+        <div className="container-component-item"><ThemeSwitch /></div>
+        <div className="container-component-item"><Switch /></div>
+        <div className="col-span-2 container-component-item"><ThemeSwitchTabs /></div>
+      </div>
     </div>
   );
 }
