c8919eecc7648e106da66d796f008e19b4d4b635
Pablo <cv.juanp@gmail.com>
2024-09-17 09:19:48 -0700

    Actualización DarkMode del componente DashAlertCard

--- src/components/surfaces/cards/desktop-mobile/DashAlertCard.tsx
+++ src/components/surfaces/cards/desktop-mobile/DashAlertCard.tsx
@@ -13,38 +13,38 @@ type Props = {
 const typeOptions = {
     pendingRecharge: {
         text: "Tienes Recargas Pendientes",
-        bgColor: "bg-violet-50",
-        textColor: "text-violet-900",
-        iconMd: <SiCashapp className='text-violet-500' size={50}/>,
-        iconSm: <SiCashapp className='text-violet-500' size={25}/>
+        bgColor: "bg-violet-50 dark:bg-violet-500",
+        textColor: "text-violet-900 dark:text-violet-100",
+        iconMd: <SiCashapp className='text-violet-500 dark:text-violet-100' size={50}/>,
+        iconSm: <SiCashapp className='text-violet-500 dark:text-violet-100' size={25}/>
     },
     lowCreditPkt1: {
         text: "¡Tu crédito PKT1 se agota!",
-        bgColor: "bg-amber-50",
-        textColor: "text-amber-900",
-        iconMd: <FaMoneyCheckDollar className='text-amber-500' size={50}/>,
-        iconSm: <FaMoneyCheckDollar className='text-amber-500' size={25}/>
+        bgColor: "bg-amber-50 dark:bg-amber-500",
+        textColor: "text-amber-900 dark:text-amber-100",
+        iconMd: <FaMoneyCheckDollar className='text-amber-500 dark:text-amber-100' size={50}/>,
+        iconSm: <FaMoneyCheckDollar className='text-amber-500 dark:text-amber-100' size={25}/>
     },
     lowCredit: {
         text: "¡El saldo de tu cuenta se agota!",
-        bgColor: "bg-amber-50",
-        textColor: "text-amber-900",
-        iconMd: <FaMoneyCheckDollar className='text-amber-500' size={50}/>,
-        iconSm: <FaMoneyCheckDollar className='text-amber-500' size={25}/>
+        bgColor: "bg-amber-50 dark:bg-amber-500",
+        textColor: "text-amber-900 dark:text-amber-100",
+        iconMd: <FaMoneyCheckDollar className='text-amber-500 dark:text-amber-100' size={50}/>,
+        iconSm: <FaMoneyCheckDollar className='text-amber-500 dark:text-amber-100' size={25}/>
     },
     invoices: {
         text: "Tienes facturas que están por vencer",
-        bgColor: "bg-teal-50",
-        textColor: "text-teal-900",
-        iconMd: <FaFileInvoice className='text-teal-500' size={50}/>,
-        iconSm: <FaFileInvoice className='text-teal-500' size={25}/>
+        bgColor: "bg-teal-50 dark:bg-teal-500",
+        textColor: "text-teal-900 dark:text-teal-100",
+        iconMd: <FaFileInvoice className='text-teal-500 dark:text-teal-100' size={50}/>,
+        iconSm: <FaFileInvoice className='text-teal-500 dark:text-teal-100' size={25}/>
     },
     congrats: {
         text: "¡Felicidades, estás al día",
-        bgColor: "bg-green-50",
-        textColor: "text-green-900",
-        iconMd: <FaCheckCircle className='text-green-500' size={50}/>,
-        iconSm: <FaCheckCircle className='text-green-500' size={25}/>
+        bgColor: "bg-green-50 dark:bg-green-500",
+        textColor: "text-green-900 dark:text-green-100",
+        iconMd: <FaCheckCircle className='text-green-500 dark:text-green-100' size={50}/>,
+        iconSm: <FaCheckCircle className='text-green-500 dark:text-green-100' size={25}/>
     }
 }
 export default function DashAlertCard({size,type }: Props) {
