a0382e7375cd7ee2550e5572740a49715d8b5f24
Pablo <cv.juanp@gmail.com>
2024-09-18 16:07:51 -0700

    Agregado componente OsStatus

---
+++ src/components/data-display/onsite/OsStatus.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import type {ShipmentStatus} from "@/types/shipments.types"
+import { Status } from './Status'
+type Props = {
+    status: ShipmentStatus
+}
+type StatusColor = "warning" | "blue" | "success" | "default" | "primary" | "secondary" | "danger" | undefined;
+
+const statusOptions: Record<ShipmentStatus, { color: StatusColor }> = {
+  "en sitio": { color: "warning" },
+  "en tránsito": { color: "blue" },
+  "entregado": { color: "success" },
+  "cancelado": { color: "danger" },
+};
+export default function OsStatus({status}: Props) {
+    const {color} = statusOptions[status]
+  return (
+   <Status  color={color}><span className='capitalize'>{status}</span></Status>
+  )
+}
