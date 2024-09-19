09b977425303ad5570ffafe66b9f62fcd9fbcdba
Pablo <cv.juanp@gmail.com>
2024-09-19 15:22:03 -0700

    Actualizado el estilo de AddressTableCard

--- src/components/surfaces/cards/desktop/AddressTableCard.tsx
+++ src/components/surfaces/cards/desktop/AddressTableCard.tsx
@@ -4,29 +4,29 @@ import React from 'react'
 import { FaEdit, FaTrashAlt } from 'react-icons/fa'
 
 type Props = {
-  address:string,
-  postalCode:string,
-  location:string,
-  country:string,
+  address: string,
+  postalCode: string,
+  location: string,
+  country: string,
 }
 
-export default function AddressTableCard({address,postalCode,location,country}: Props) {
+export default function AddressTableCard({ address, postalCode, location, country }: Props) {
   return (
-    <Card className='flex-row shadow-md min-h-24'>
-    <CardBody className='flex-row py-4'>    
-        <div className=' ml-3'>
-            <p className="font-semibold">{address}</p>
-            <p className="text-xs font-medium">{location}, {country}, {postalCode}</p>
+    <Card className='flex-row shadow-sm min-h-28'>
+      <CardBody className='flex-row py-4'>
+        <div className=' ml-3 flex flex-col justify-center'>
+          <p className="font-semibold">{address}</p>
+          <p className="text-xs font-medium">{location}, {country}, {postalCode}</p>
         </div>
-        <div className='flex  ml-auto gap-1'>
-        <Button isIconOnly size='sm' radius='full' className='bg-blue-100 dark:bg-blue-600/50'>
-                    <FaEdit className='text-blue-600 dark:text-blue-100' size={14} />
-                </Button>
-                <Button isIconOnly size='sm' radius='full' className='bg-red-100 dark:bg-red-600/50'>
-                    <FaTrashAlt className='text-red-500 dark:text-red-100' size={14} />
-                </Button>
+        <div className='flex flex-col justify-center ml-auto gap-1'>
+          <Button isIconOnly size='sm' variant='light' radius='full'>
+            <FaEdit className='text-blue-600' size={14} />
+          </Button>
+          <Button isIconOnly size='sm' variant='light' radius='full'>
+            <FaTrashAlt className='text-red-500 ' size={14} />
+          </Button>
         </div>
-    </CardBody>
-</Card>
+      </CardBody>
+    </Card>
   )
 }
