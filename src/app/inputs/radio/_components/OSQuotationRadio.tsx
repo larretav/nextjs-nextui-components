c57c02d29c32f3bb5f55dfe2bd61c053a1ba5104
Alejandro Larreta Vzla <larreta_vzla@hotmail.com>
2025-01-30 23:20:27 -0700

    Arreglado el checkbox del QuotationRadio

--- src/app/inputs/radio/_components/OSQuotationRadio.tsx
+++ src/app/inputs/radio/_components/OSQuotationRadio.tsx
@@ -1,14 +1,63 @@
 'use client';
 
 import { QuotationRadio } from '@/components'
+import { Shippers } from '@/types';
+import { Button } from '@nextui-org/button';
 import { Checkbox } from '@nextui-org/checkbox';
+import { Popover, PopoverContent, PopoverTrigger } from '@nextui-org/popover';
 import { RadioGroup } from '@nextui-org/radio'
 import React, { useState } from 'react'
+import { FaCircleExclamation } from 'react-icons/fa6';
+
+interface Quotation {
+  id: number;
+  cost: number;
+  normalCost: number;
+  deliveredType: string;
+  estimateDate: string;
+  shipper: Shippers;
+}
 
 export const OSQuotationRadio = () => {
 
+  const quotations: Quotation[] = [
+    {
+      id: 1,
+      cost: 250.5,
+      normalCost: 200.0,
+      deliveredType: "Standard",
+      estimateDate: "Miercoles 8 de septiembre",
+      shipper: "fedex",
+    },
+    {
+      id: 2,
+      cost: 250.5,
+      normalCost: 200.0,
+      deliveredType: "Standard",
+      estimateDate: "Miercoles 8 de septiembre",
+      shipper: "fedex",
+    },
+    {
+      id: 3,
+      cost: 250.5,
+      normalCost: 200.0,
+      deliveredType: "Standard",
+      estimateDate: "Miercoles 8 de septiembre",
+      shipper: "fedex",
+    },
+    {
+      id: 4,
+      cost: 350.5,
+      normalCost: 600.0,
+      deliveredType: "Standard",
+      estimateDate: "Miercoles 8 de septiembre",
+      shipper: "fedex",
+    },
+  ]
+
   const [selected, setSelected] = useState<string | undefined>()
-  const [isSelected, setIsSelected] = useState<boolean>(false)
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+  const [errorOpen, setErrorOpen] = useState([])
 
   const handleChange = (value: string) => {
     setSelected(value)
@@ -18,24 +67,49 @@ export const OSQuotationRadio = () => {
   return (
     <>
       <RadioGroup className="gap-3" value={selected} onValueChange={handleChange}>
-        <QuotationRadio value="fedex" cost={250.5} normalCost={200.0} deliveredType="Standard" estimateDate="Miercoles 8 de septiembre" shipper="fedex" />
+        {
+          quotations.map(({ id, cost, normalCost, deliveredType, estimateDate, shipper }) => (
+            <QuotationRadio
+              key={id}
+              value={shipper}
+              cost={cost}
+              normalCost={normalCost}
+              deliveredType={deliveredType}
+              estimateDate={estimateDate}
+              shipper={shipper}
+            />
+          ))
+        }
+
+        <QuotationRadio
+          value="fedex"
+          cost={250.5}
+          normalCost={200.0}
+          deliveredType="Standard"
+          estimateDate="Miercoles 8 de septiembre"
+          shipper="fedex"
+          isDisabled
+          errorContent={<div className="w-full h-full flex justify-end bg-content1/80 p-3">
+            <Popover placement="left-start">
+              <PopoverTrigger>
+                <div>
+                  <FaCircleExclamation size="1.25rem" className="text-warning-500" />
+                </div>
+              </PopoverTrigger>
+              <PopoverContent >
+                <div className="px-1 py-2">
+                  <div className="text-small font-bold">Popover Content</div>
+                  <div className="text-tiny">This is the popover content</div>
+                </div>
+              </PopoverContent>
+            </Popover>
+          </div>}
+        />
         <QuotationRadio value="pkt1" cost={250.5} normalCost={200.0} deliveredType="Standard" estimateDate="Miercoles 8 de septiembre" shipper="pkt1" />
         <QuotationRadio value="paquetexpress" cost={250.5} normalCost={200.0} deliveredType="Standard" estimateDate="Miercoles 8 de septiembre" shipper="paquetexpress" />
         <QuotationRadio value="dhl" cost={250.5} normalCost={200.0} deliveredType="Standard" estimateDate="Miercoles 8 de septiembre" shipper="dhl" />
         <QuotationRadio value="ups" cost={250.5} normalCost={200.0} deliveredType="Standard" estimateDate="Miercoles 8 de septiembre" shipper="ups" />
       </RadioGroup>
-
-      <div className="flex justify-center items-center gap-2 outline">
-        <Checkbox
-          isSelected={isSelected}
-          color="success"
-          radius="sm"
-          className="z-50"
-          onValueChange={setIsSelected}
-          onClick={() => { console.log('first click') }}
-          className="outline"
-        />
-      </div>
     </>
   )
 }
--- src/components/inputs/radio/QuotationRadio.tsx
+++ src/components/inputs/radio/QuotationRadio.tsx
@@ -5,7 +5,9 @@ import { Shippers } from "@/types";
 import { Checkbox } from "@nextui-org/checkbox";
 import { RadioProps, useRadio } from "@nextui-org/radio";
 import { VisuallyHidden } from "@react-aria/visually-hidden";
+import clsx from "clsx";
 import { get } from "http";
+import { ReactNode } from "react";
 
 type Props = RadioProps & {
   cost: number;
@@ -13,26 +15,27 @@ type Props = RadioProps & {
   estimateDate?: string;
   deliveredType?: string;
   shipper: Shippers;
+  errorContent?: ReactNode;
 }
 
-export const QuotationRadio = ({ cost = 0.0, normalCost = 0.0, estimateDate, deliveredType, shipper, ...restProps }: Props) => {
+export const QuotationRadio = ({ cost = 0.0, normalCost = 0.0, estimateDate, deliveredType, shipper, errorContent, ...restProps }: Props) => {
   const {
     Component,
     isSelected,
     getBaseProps,
     getInputProps,
-    getControlProps,
-    getWrapperProps,
     isDisabled,
   } = useRadio(restProps);
 
   return (
     <Component
       {...getBaseProps()}
-      className={cn(
-        "group inline-flex items-center justify-between bg-content1 hover:bg-content2 shadow-md transition-background",
-        "w-fit cursor-pointer border border-transparent rounded-2xl gap-4 p-3 pr-2 z-0",
-        "data-[selected=true]:border-primary",
+      className={clsx(
+        "group inline-flex items-center justify-between bg-content1 hover:bg-content2 shadow-md transition-background relative overflow-hidden",
+        "w-full cursor-pointer border border-transparent rounded-2xl gap-4 p-3 pr-2 z-0",
+        "data-[selected=true]:border-primary", {
+          'cursor-auto hover:bg-content1' : isDisabled
+        }
       )}
 
     >
@@ -40,31 +43,34 @@ export const QuotationRadio = ({ cost = 0.0, normalCost = 0.0, estimateDate, del
         <input {...getInputProps()} />
       </VisuallyHidden>
 
-      <div className="flex flex-col gap-4 w-full ">
-        <div className="flex gap-3 ">
-          <ShipperType shipper={shipper} />
-          <div className="flex flex-col">
-            <div className="text-4xl font-semibold text-nowrap">${cost.toFixed(2)}<span className="text-base font-bold">MXN</span></div>
-            <span className="text-xs font-medium text-default-400">Precio normal: ${normalCost.toFixed(2)}</span>
+      {errorContent && <div className="absolute top-0 left-0 w-full h-full z-50">{errorContent}</div>}
+
+      <div className="w-full flex items-center justify-between gap-2">
+        <div className="flex flex-col gap-4 w-full ">
+          <div className="flex gap-3 ">
+            <ShipperType shipper={shipper}/>
+            <div className="flex flex-col">
+              <div className="text-4xl font-semibold text-nowrap">${cost.toFixed(2)}<span className="text-base font-bold">MXN</span></div>
+              <span className="text-xs font-medium text-default-400">Precio normal: ${normalCost.toFixed(2)}</span>
+            </div>
           </div>
+
+          <div className="flex flex-col">
+            <div className="text-base font-medium ">Entrega estimada: <span className="font-bold">{estimateDate}</span></div>
+            <span className="text-sm font-medium text-default-400">{deliveredType}</span>
+          </div>
+
         </div>
 
-        <div className="flex flex-col">
-          <div className="text-base font-medium ">Entrega estimada: <span className="font-bold">{estimateDate}</span></div>
-          <span className="text-sm font-medium text-default-400">{deliveredType}</span>
-        </div>
-
+        {
+          !isDisabled && <Checkbox
+            isSelected={isSelected}
+            color="primary"
+            radius="sm"
+            className="-z-10 "
+          />
+        }
       </div>
-
-
-      <Checkbox
-        isSelected={isSelected}
-        color="success"
-        radius="sm"
-        className="z-50"
-        onValueChange={() => { restProps.onChange?.({ target: { value: restProps.value } } as any)}}
-      />
-
     </Component>
   );
 };
