a053ce6e12c031f3c5afd481c2c0507612de7282
Alejandro Larreta Vzla <larreta_vzla@hotmail.com>
2025-01-31 23:40:21 -0700

    BranchDeliverRadio terminado creo

--- src/app/inputs/radio/_components/OSBranchDeliverRadio.tsx
+++ src/app/inputs/radio/_components/OSBranchDeliverRadio.tsx
@@ -1,10 +1,57 @@
 'use client';
 
 import { BranchDeliverRadio } from '@/components';
+import { Shippers } from '@/types';
 import { RadioGroup } from '@nextui-org/radio';
 import React, { useState } from 'react'
 
+
+
 export const OSBranchDeliverRadio = () => {
+
+  const shipper: Shippers = "paquetexpress";
+
+  const branches = [
+    {
+      "key": "LMM03",
+      "name": "LOS MOCHIS 3",
+      "branchType": "G",
+      "zipCode": "81280",
+      "colony": "BIENESTAR",
+      "street": "AV. SANTOS DEGOLLADO SUR",
+      "number": "665",
+      "city": "LOS MOCHIS",
+      "state": "SINALOA",
+      "municipality": "AHOME",
+      "country": "MEXICO"
+    },
+    {
+      "key": "LMM01",
+      "name": "LOS MOCHIS 1",
+      "branchType": "G",
+      "zipCode": "81256",
+      "colony": "PARQUE INDUSTRIAL ECOLOGICO",
+      "street": "CALLE DE LA AGRICULTURA",
+      "number": "972",
+      "city": "LOS MOCHIS",
+      "state": "SINALOA",
+      "municipality": "AHOME",
+      "country": "MEXICO"
+    },
+    {
+      "key": "LMM05",
+      "name": "LOS MOCHIS 5",
+      "branchType": "G",
+      "zipCode": "81278",
+      "colony": "FRANCISCO VILLA",
+      "street": "BLVD. CENTENARIO ESQUINA CON BLVD. CANUTO IBARRA",
+      "number": "SN",
+      "city": "LOS MOCHIS",
+      "state": "SINALOA",
+      "municipality": "AHOME",
+      "country": "MEXICO"
+    }
+  ]
 
   const [selected, setSelected] = useState<string | undefined>()
 
@@ -15,12 +62,16 @@ export const OSBranchDeliverRadio = () => {
   return (
     <>
       <RadioGroup className="gap-3" value={selected} onValueChange={handleChange}>
-        <BranchDeliverRadio
-          value="0"
-          shipper="fedex"
-          branchName="LMM - GUASAVE"
-          address="Av. Francisco I. Madero #2314, Guasave, Guasave, Sinaloa, México."
-        />
+        {
+          branches.map(({key, name, branchType, ...address}) => (
+            <BranchDeliverRadio
+              value={key}
+              shipper={shipper}
+              branchName={name}
+              address={address}
+            />
+          ))
+        }
       </RadioGroup>
 
     </>
--- src/components/inputs/radio/BranchDeliverRadio.tsx
+++ src/components/inputs/radio/BranchDeliverRadio.tsx
@@ -1,23 +1,37 @@
 'use client';
 import { ShipperType } from "@/components/data-display/onsite/ShipperType";
 import { Shippers } from "@/types";
+import { Button } from "@nextui-org/button";
 import { Checkbox } from "@nextui-org/checkbox";
 import { Link } from "@nextui-org/link";
 import { RadioProps, useRadio } from "@nextui-org/radio";
 import { VisuallyHidden } from "@react-aria/visually-hidden";
 import clsx from "clsx";
 import { ReactNode } from "react";
+import { FaArrowUpRightFromSquare, FaLocationDot, FaSquareArrowUpRight } from "react-icons/fa6";
+
+export interface BranchDeliverAddress {
+  zipCode: string,
+  colony: string,
+  street: string,
+  number: string,
+  city: string,
+  state: string,
+  municipality: string,
+  country: string,
+}
 
 type Props = RadioProps & {
   branchName: string;
-  address: string;
+  address: BranchDeliverAddress;
   shipper: Shippers;
 }
 
 export const BranchDeliverRadio = ({ branchName, address, shipper, ...restProps }: Props) => {
 
-  // const urlAddrs = `https://www.google.com/maps/search/?api=1&query=Colonia+Numero,+Ciudad,+Municipio,+Estado,+Pais`
-  const urlAddrs = `https://www.google.com/maps/search/?api=1&query=${address}`
+  const urlAddrs = `https://www.google.com/maps/search/?api=1&query=${address.colony}+${address.street},+${address.city},+${address.municipality},+${address.state},+${address.country}`
+  // const urlAddrs = `https://www.google.com/maps/search/?api=1&query=${address}`
+
 
   const {
     Component,
@@ -42,30 +56,41 @@ export const BranchDeliverRadio = ({ branchName, address, shipper, ...restProps 
         <input {...getInputProps()} />
       </VisuallyHidden>
 
-      <div className="w-full h-full flex items-center justify-between relative">
-        <div className="flex flex-col h-full gap-4 ">
+      <div className="w-full h-full flex items-center justify-between">
+        <div className="flex flex-col h-full gap-4">
 
           <div className="flex gap-3">
             <ShipperType shipper={shipper} />
             <div className="flex flex-col">
               <p className="text-lg font-semibold">{branchName}</p>
-              <Link href={urlAddrs} target="_blank" className="text-sm font-semibold">{address}</Link>
+              <p className="text-sm text-foreground-500">{address.colony} #{address.number}, {address.city}, {address.municipality}, {address.state}, {address.country}</p>
             </div>
           </div>
         </div>
 
-        {
-          !isDisabled &&
+        <div className="flex items-center gap-1">
+          <Button
+            as={Link}
+            href={urlAddrs}
+            target="_blank"
+            isIconOnly
+            variant="light"
+            radius="full"
+            size="sm"
+            className="text-blue-400 dark:text-default-500"
+          >
+            <FaLocationDot size={16} />
+          </Button>
           <Checkbox
             isSelected={isSelected}
             color="primary"
             radius="sm"
-            className="-z-10 -m-0 p-1 absolute top-0 bottom-0 right-0"
+            className="-z-10 -m-0 p-1 "
             classNames={{
               wrapper: "me-0"
             }}
           />
-        }
+        </div>
       </div>
     </Component >
   );
